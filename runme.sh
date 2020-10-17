6a0b11ae9deecedaa3b56951b20bf436fae4785d
Kingdon Barrett <kingdon.b@nd.edu>
2020-10-17 11:56:04 -0400

    fixup

--- runme.sh
+++ runme.sh
@@ -4,16 +4,17 @@
 # ### of the Bash interpreter: https://unix.stackexchange.com/a/314379/229312
 
 # This way does not handle failures, so be sure that map.json makes sense:
-keys=( $(jq '.git_repo | keys_unsorted | .[]' map.json) )
-repos=( $(jq '.git_repo | to_entries | .[].value' map.json) )
-normals=( $(jq '.["normalize-hyphenate"] | to_entries | .[].value' map.json) )
+keys=( $(jq '.git_repo | keys_unsorted | .[]' map.json --raw-output) )
+repos=( $(jq '.git_repo | to_entries | .[].value' map.json --raw-output) )
+normals=( $(jq '.["normalize-hyphenate"] | to_entries | .[].value' map.json --raw-output) )
 
 len=${#keys[@]}
 
 for (( i=0; i<$len; i++ )); do
-  echo git clone $repos[$i] $normals[$i]
-  pushd $normals[$i]
-  tar xvf ../Jenkinsfile.tar.gz
+  # echo git clone ${repos[$i]} ${normals[$i]}
+  git submodule add ${repos[$i]} ${keys[$i]}
+  pushd ${keys[$i]}
+  tar xvf ../Jenkinsfile-example.tar.gz
   bash --login
   popd
 done
